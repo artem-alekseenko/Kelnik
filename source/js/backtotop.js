ed941243d821343e0187d49514dbfe105bd2dcf1
Artem Alekseenko <ivanovo.cwi@gmail.com>
2019-08-08 20:07:54 +0300

    add backToTop button

---
+++ source/js/backtotop.js
@@ -0,0 +1,21 @@
+const backToTop = () => {
+    const backToTopButton = document.querySelector(".backtotop")
+
+    window.onscroll = () => {
+        const scrollTop = document.documentElement.scrollTop        
+        if (scrollTop > 100) {
+            backToTopButton.classList.add("backtotop--visible")
+        } else {
+            backToTopButton.classList.remove("backtotop--visible")
+        }
+    }
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    })
+}
+
+backToTop()
